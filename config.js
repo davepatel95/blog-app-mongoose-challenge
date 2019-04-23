7a95827d6e9c33d86a81c34336d5297dad9f1a10
Dave Patel <davepatel95@gmail.com>
2019-04-23 14:27:07 -0700

    seeded data

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 'use strict';
-
-exports.DATABASE_URL = "mongodb://localhost/blog-app-mongoose-challenge";
-exports.PORT = 4040;
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blog-app-mongoose-challenge";
+exports.PORT = process.env.PORT || 8080;
